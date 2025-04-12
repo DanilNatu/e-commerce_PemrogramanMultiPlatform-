5062aaace5167a09b4bb38361e4fcdc9ee706a48
rioadityapratama <rioaditya444@gmail.com>
2025-04-12 21:36:51 +0800

    membuat halamandetail produk

---
+++ lib/detail.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:project2/keranjang.dart';
+
+class ProductDetailPage extends StatefulWidget {
+  @override
+  _ProductDetailPageState createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState extends State<ProductDetailPage> {
+  String selectedSize = 'null';
+  final List<String> sizes = ['S', 'M', 'L', 'XL'];
+  bool _isfavorite = false;
+
+  int amount = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    final double unitPrice = 125.000;
+    final double total = unitPrice * amount;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Produk'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Image.network(
+                'https://contents.mediadecathlon.com/p2157319/k45a8143f29ae498e05be9c1588d95135/kaos-running-dry-fit-baju-lari-pria-breathable-hitam-decathlon-8488034.jpg?f=1920x0&format=auto',
+                height: 250,
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: double.infinity,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.black26)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Adidas Tshirt Oversize',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Deskripsi',
+                    style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Text(
+                      'Material : cotton 24 s\nPlastisol ink\nOversize Regular\nUnisex',
+                      style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Pilih Ukuran:',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      _isfavorite ? Icons.favorite : Icons.favorite_border,
+                      color: _isfavorite ? Colors.red : Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isfavorite = !_isfavorite;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Wrap(
+                  alignment: WrapAlignment.start,
+                  spacing: 8,
+                  children: sizes.map((size) {
+                    return ChoiceChip(
+                      label: Text(size),
+                      selected: selectedSize == size,
+                      onSelected: (selected) {
+                        setState(() {
+                          selectedSize = size;
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Row(
+                children: [
+                  const Text(
+                    'Jumlah:',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 3),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 203, 203, 203),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(
+                          width: 35,
+                          height: 33,
+                          child: IconButton(
+                            icon: const Icon(Icons.remove),
+                            iconSize: 18,
+                            onPressed: () {
+                              if (amount > 1) {
+                                setState(() {
+                                  amount--;
+                                });
+                              }
+                            },
+                          ),
+                        ),
+                        Text('$amount'),
+                        SizedBox(
+                          width: 35,
+                          height: 33,
+                          child: IconButton(
+                            icon: const Icon(Icons.add),
+                            iconSize: 18,
+                            onPressed: () {
+                              setState(() {
+                                amount++;
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    'Rp ${total.toStringAsFixed(3)}',
+                    style: const TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.shopping_cart),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => KeranjangScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 123, 138, 215),
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                      ),
+                      child: const Text(
+                        "> > > > > > > Swipe",
+                        style: TextStyle(fontSize: 17, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
